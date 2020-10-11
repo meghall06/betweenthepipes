c6f98635d2ddd2b1b0d02b414687c314cb689567
meghall06 <mhall2012@gmail.com>
2020-10-11 18:01:57 -0400

    using data.R instead

--- R/data.R
+++ R/data.R
@@ -1,3 +1,20 @@
+#' @importFrom tibble tibble
+NULL
+
+#' Sample of NHL play-by-play data.
+#'
+#' A dataset containing NHL play-by-play data for four Philadelphia
+#' Flyers games from November 2019.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source Scraped via the Evolving-Hockey scraper \url{https://github.com/evolvingwild/evolving-hockey/blob/master/EH_scrape_functions.R}
+"pbp_example"
+
 #' Sample of biographic NHL data.
 #'
 #' A dataset containing some NHL biographic data from 2019, useful for
@@ -25,5 +42,3 @@
 #'
 #' @source \url{http://www.naturalstattrick.com/}
 "bio_example"
-
-NULL
