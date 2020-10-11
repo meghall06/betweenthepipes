7e651716c2c20e4a1fb2f392f1851b9a27640741
meghall06 <mhall2012@gmail.com>
2020-10-11 14:42:11 -0400

    testing data

---
+++ R/bio_example.R
@@ -0,0 +1,13 @@
+#' Sample of biographic NHL data.
+#'
+#' A dataset containing some NHL biographic data from 2019, useful for
+#' practicing joins with the data in pbp_example.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.naturalstattrick.com/}
+"bio_example"
---
+++ R/pbp_example.R
@@ -0,0 +1,13 @@
+#' Sample of NHL play-by-play data.
+#'
+#' A dataset containing NHL play-by-play data for four Philadelphia
+#' Flyers games from November 2019.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source Scraped via the Evolving-Hockey scraper \url{https://github.com/evolvingwild/evolving-hockey/blob/master/EH_scrape_functions.R}
+"pbp_example"
